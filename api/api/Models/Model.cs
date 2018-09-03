728bf45014120cb9517fe6eed1fa3f7b4ecde859
Benz <benz@git.abc>
2018-09-03 13:13:48 +0700

    Initialize project

---
+++ api/api/Models/Model.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace api.Models
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options)
+        : base(options)
+        { }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+    }
+
+
+    #region Models Class
+
+    public class Department
+    {
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+    }
+
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public int EmployeeAge { get; set; }
+
+        public int DepartmentId { get; set; }
+        public Department Department { get; set; }
+    }
+    #endregion
+}
